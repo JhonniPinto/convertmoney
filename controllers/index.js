ea80cad25d832305fed774be6bbcde4bcf160d8d
JhonniPinto <jhonnipinto@gmail.com>
2019-07-31 19:28:42 -0300

    Revert "converts version 2"
    This reverts commit db91e850bac13cafc28bf02ae5156e45e852cbc6.

--- controllers/index.js
+++
@@ -1,8 +0,0 @@
-const apiBCB = require('../lib/api.bcb')
-
-const index = async(req, res) => {
-    const cotacao = await apiBCB.getQuotation()
-    res.render('index', { cotacao, quantidade: false , err: false })
-}
-
-module.exports = index
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router     = require('express').Router()
-const index      = require('../controllers/index')
-const convertido = require('../controllers/convertido')
-
-router.get('/', index)
-
-router.get('/convertido', convertido)
-
-module.exports = router
